3b923cac640894c6eb73460be6c888caafe4759a
irfan <irfan@DESKTOP-QBL2BIE.localdomain>
2022-01-06 03:29:47 +0700

    Update : + gitignore + composer

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/autoload', [Controller::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
